ee0028ca04b76c54174a45414deb456084903855
JeanLamego <jeanlamego@hotmail.com>
2023-08-22 12:23:33 -0300

    Initial commit

---
+++ app/src/main/java/com/salvatoreacademy/primeira_aula_salvatore_academy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.salvatoreacademy.primeira_aula_salvatore_academy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
